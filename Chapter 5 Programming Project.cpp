43532be6af58a78800fe743a3cf359b0966d85fc
RScott99 <rsh06.rh@gmail.com>
2025-07-12 11:03:38 -0400

    finished code and it seems to work as intended

--- Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project.cpp
@@ -8,16 +8,16 @@ using namespace std;
 int main()
 {
     //Initilizing Variables
-    int popSizeInitial, popSizeFinal;
-    double days, growthRate;
+    int days;
+    double currentPopulation, growthRate;
 
     //querying for popSizeInitial
     std::cout << "Enter the initial population: ";
-    std::cin >> popSizeInitial;
+    std::cin >> currentPopulation;
     //validating popSizeInitial is not less than 2
-    while (popSizeInitial < 2) {
+    while (currentPopulation < 2) {
         std::cout << "Please enter an integer greater than 1: ";
-        std::cin >> popSizeInitial;
+        std::cin >> currentPopulation;
     }
     //querying growthRate
     std::cout << "Enter the average growth rate (percentage): ";
@@ -33,12 +33,17 @@ int main()
     std::cout << "Enter the number of days to multiply: ";
     std::cin >> days;
     //validating that days is not < 1
-    while (days < 1.0) {
+    while (days < 1) {
         std::cout << "Please enter a value greater than 1: ";
         std::cin >> days;
     }
-
-
-
+    //outputing day 0 pop.
+    std::cout << "Population Size on day 0: " << currentPopulation << endl;
+    //iterating all days and outputting each one
+    for (int i = 1; i <= days; i++) {
+        currentPopulation *= (1.0 + growthRate);
+        std::cout << "Population Size on day " << i << ": " << currentPopulation << endl;
+    }
+    
     return 0;
 }
