d6e7e014dd174ddab02359c5da68b18ebfe0e9f9
RScott99 <rsh06.rh@gmail.com>
2025-07-12 12:25:51 -0400

    fixed spelling errors

--- Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project.cpp
@@ -10,15 +10,15 @@ using namespace std;
 int main()
 {    
     std::cout << "Pattern A:\n";//header for pattern a
-    for (int i = 1; i <= 10; i++) {//i = what row the patter is on (i=1 is first row) & itterates through until 10
-        for (int j = 1; j <= i; j++) {//j = how many asterisks per row. (j=1 is 1 '*')& itterates through until 10
+    for (int i = 1; i <= 10; i++) {//i = what row the patter is on (i=1 is first row) & iterates through until 10
+        for (int j = 1; j <= i; j++) {//j = how many asterisks per row. (j=1 is 1 '*')& iterates through until 10
             std::cout << "*";//outputs the asterisk
         }
         std::cout << endl;//returns to next line
     }
     std::cout << "\nPatter B:\n";//header for pattern b
     for (int i = 1; i <= 10; i++) {//same as line 13
-        for (int j = 10; j >= i; j--) {//reverse of like 14, so it starts at 10 asterisks and itterates down until 1
+        for (int j = 10; j >= i; j--) {//reverse of like 14, so it starts at 10 asterisks and iterates down until 1
             std::cout << "*";//outputs the asterisk
         }
         std::cout << endl;//returns to next line
